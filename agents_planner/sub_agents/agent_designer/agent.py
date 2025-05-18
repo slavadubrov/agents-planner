b5f874cb1a5a639342984a5d5ac7b39209763288
Viacheslav Dubrov <slavadubrov@gmail.com>
2025-05-18 21:36:41 +0200

    add load_web_page

--- agents_planner/sub_agents/agent_designer/agent.py
+++ agents_planner/sub_agents/agent_designer/agent.py
@@ -3,7 +3,8 @@ This agent is responsible for designing appropriate agents for each subtask.
 """
 
 from google.adk.agents.llm_agent import LlmAgent
-from google.adk.tools import google_search
+from google.adk.tools.function_tool import FunctionTool
+from google.adk.tools.load_web_page import load_web_page
 
 from ... import MODEL
 from ...modules.constants import STATE_AGENT_DESCRIPTIONS
@@ -11,11 +12,13 @@ from .prompt import (
     INSTRUCTION,
 )
 
+web_loader = FunctionTool(load_web_page)
+
 agent_designer_agent = LlmAgent(
     name="AgentDesignerAgent",
     model=MODEL,
     instruction=INSTRUCTION,
     description="Designs appropriate agents for each subtask.",
-    tools=[google_search],
+    tools=[web_loader],
     output_key=STATE_AGENT_DESCRIPTIONS,
 )
